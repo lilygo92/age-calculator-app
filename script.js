6bf4ba36092b1a5e0d9e8ab0d1e8c4cf708c313a
Lily Go <lilygo7192@gmail.com>
2023-09-23 21:11:24 -0700

    Setup click handler for svg button

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-getAge = (birthday) => {
-  const today = new Date()
-  let birthdate = new Date(birthday);
-  let year = today.getFullYear() - birthdate.getUTCFullYear();
-  let month = today.getMonth() - birthdate.getUTCMonth();
-  let day = today.getDate() - birthdate.getUTCDate();
-
-  if (month === 0 && today.getDate() < birthdate.getUTCDate()) {
-    year--;
-  } 
-  else if (month < 0) {
-    year--;
-    month = 12 + month;
+$(document).ready(() => {
+  getAge = (birthday) => {
+    const today = new Date()
+    let birthdate = new Date(birthday);
+    let year = today.getFullYear() - birthdate.getUTCFullYear();
+    let month = today.getMonth() - birthdate.getUTCMonth();
+    let day = today.getDate() - birthdate.getUTCDate();
+  
+    if (month === 0 && today.getDate() < birthdate.getUTCDate()) {
+      year--;
+    } 
+    else if (month < 0) {
+      year--;
+      month = 12 + month;
+    }
+  
+    return [year, month, day];
   }
+  
+  $("#enter-button").on("click", () => {
+    console.log("b");
 
-  return [year, month, day];
-}
+    if ($("#day").val() === "" || $("#month").val() === "" || $("#year").val() === "") {
+      console.log("a");
+    } else {
+    }
+  })
 
-console.log(getAge("2002-07-19"));
+
+})
